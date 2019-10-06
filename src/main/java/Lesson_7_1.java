b75912215c0f4b95097a1d240d09029bce9303e3
oleh <oleg1987kiev@gmail.com>
2019-10-06 10:30:47 +0300

    Initial commit

---
+++ src/main/java/Lesson_7_1.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class Lesson_7_1 {
+    public static void main(String[] args){
+        {
+
+            // create a ArrayList String type
+            ArrayList<String> gfg = new ArrayList<String>();
+
+            // Initialize an ArrayList with add()
+            gfg.add("Geeks");
+            gfg.add("for");
+            gfg.add("Geeks");
+
+            // print ArrayList
+            System.out.println("ArrayList : " + gfg);
+        }
+    }
+}
