24fb0344fe63a73a7c13c7745a247581c39274d9
oleh <oleg1987kiev@gmail.com>
2019-10-07 00:28:37 +0300

    test

--- src/main/java/Lesson_7_2.java
+++ src/main/java/Lesson_7_2.java
@@ -1,2 +1,12 @@
 public class Lesson_7_2 {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
 }
---
+++ src/main/java/Lesson_7_3.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Lesson_7_3 {
+    public static void main(String[] args) {
+        // Create a HashMap object called capitalCities
+        HashMap<String, Integer> capitalCities = new HashMap<String, Integer>();
+
+        // Add keys and values (Country, City)
+        capitalCities.put("BMW", 5);
+
+        System.out.println(capitalCities);
+    }
+}
